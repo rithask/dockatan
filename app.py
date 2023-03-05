9125b151cf8274b7af33da0f9967fd59d38b7721
Rithas K <rithask@icloud.com>
2023-03-05 16:25:13 +0530

    Add flash message if the user try to delete an image of a running ocntainer

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from docker_commands import *
 import requests
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello_world():
@@ -32,7 +34,13 @@ def containers():
 	exited = list_terminated_container()
 	return render_template('containers.html', running=running, exited=exited)
 
-@app.route('/images')
+@app.route('/images', methods=['GET', 'POST'])
 def images():
+	if request.method == "POST":
+		image_name = f"{request.form['repo']}:{request.form['tag']}"
+		if check_running_containers(image_name) == True:
+			flash(f"A running container is using the image \"{image_name}\"")
+		else:
+			delete_image(image_name)
 	images = list_images()
 	return render_template('images.html', images=images)
